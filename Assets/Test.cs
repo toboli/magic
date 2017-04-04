54c8e2fbdb37bf63f41e99912de805cc5b03640b
渡部かすみ <kasumisheep@gmail.com>
2017-04-04 23:18:13 +0900

    1行目：C#スクリプト　配列の練習とBossクラスの追加 2行目： 3行目：配列の練習と、BOSSクラスの内容追加をしました。

---
+++ Assets/Test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    int mp = 53;
+    public void Magic(int magic){
+        this.mp -= magic;
+
+        if (mp >= 5)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+
+    }
+    //Magic関数内でmpを5減らし、コンソールに
+    //「魔法攻撃をした。残りMPは〇〇。」と表示してください。
+    //mpが足りない場合、「MPが足りないため魔法が使えない。」と表示
+
+}
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+        Boss magic = new Boss();
+
+        for (int i= 0; i<11; i++)
+        {
+            magic.Magic(5);
+        }
+
+
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length; i > 0; i--)
+        {
+            Debug.Log(array[i - 1]);
+        }
+
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
